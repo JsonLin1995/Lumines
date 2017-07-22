f9707b8dbb3a480bfb6efe79ebbd489cee4f09b6
JsonLin1995 <stu900277@gmail.com>
2017-07-22 16:10:36 +0800

    remove console

--- script.js
+++ script.js
@@ -824,7 +824,7 @@ function setCookie(cname, cvalue, exdays) {
     d.setTime(d.getTime() + (exdays*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-	console.log(cname + "=" + cvalue + ";" + expires + ";path=/");
+	//console.log(cname + "=" + cvalue + ";" + expires + ";path=/");
 }
 
 function getCookie(cname) {
@@ -849,7 +849,7 @@ function checkCookieBestRecord() {
         bestRecord = best;
     } 
 	else {
-        console.log("no cookie");
+        //console.log("no cookie");
 		bestRecord = 0;
     }
 }
